62f768d837608d130028e776993977b1e24d8646
Grego12098 <jgregory12098@gmail.com>
2023-04-25 16:04:59 +0100

    card and cardlist components

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 // import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
 function App() {
 	const navigate = useNavigate();
 	const handleClick = () => navigate('/quizzes');
